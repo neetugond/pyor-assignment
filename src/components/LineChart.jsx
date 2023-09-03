d870e5827d21f2d996f1cb0884b0bdd50add769e
neetugond <ngond92@gmail.com>
2023-09-03 21:57:54 +0530

    styling done

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -3,25 +3,26 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import axios from 'axios'
 import * as echarts from 'echarts'
+import './style.css'
 
 const LineChart = () => {
     const [dropdownSelect, setDropdownSelect] = useState('ethereum');
     const [chartData, setChartData] = useState([])
 
     useEffect(() => {
-        fetchData(); 
+        fetchData();
     }, [dropdownSelect])
-    
+
     const url = `https://api.coingecko.com/api/v3/coins/${dropdownSelect}/market_chart?vs_currency=usd&days=365&interval=daily`
 
     const fetchData = async () => {
-       try {
-           const response = await axios.get(url);
-           const data = response.data.prices;
-           setChartData(data.slice(0,100))
-        //    console.log(data)
-           lineChart(data)
-            
+        try {
+            const response = await axios.get(url);
+            const data = response.data.prices;
+            setChartData(data.slice(0, 100))
+            //    console.log(data)
+            lineChart(data)
+
         } catch (error) {
             console.log(error)
         }
@@ -31,24 +32,24 @@ const LineChart = () => {
         const chart = echarts.init(chartElement, 'dark')
         const option = {
             tooltip: {
-              trigger: "axis",
+                trigger: "axis",
             },
             xAxis: {
-              type: "time",
-              boundaryGap: false,
+                type: "time",
+                boundaryGap: false,
             },
             yAxis: {
-              type: "value",
+                type: "value",
             },
             series: [
-              {
-                areaStyle: {},
-                type: "line",
-                data: data.map(([timestamp, value]) => ({
-                  name: new Date(timestamp).toLocaleDateString(),
-                  value: [timestamp, value],
-                })),
-              },
+                {
+                    areaStyle: {},
+                    type: "line",
+                    data: data.map(([timestamp, value]) => ({
+                        name: new Date(timestamp).toLocaleDateString(),
+                        value: [timestamp, value],
+                    })),
+                },
             ],
         };
         chart.setOption(option)
@@ -57,27 +58,27 @@ const LineChart = () => {
         <>
             <div className="main-container">
                 <div className="dropdown-main-container">
-                <select className='dropdown-select' value={dropdownSelect} onChange={(e) => setDropdownSelect(e.target.value)}>
-                    <option value="bitcoin">Bitcoin</option>
-                    <option value="filecoin">Filecoin</option>
-                    <option value="ethereum">Ethereum</option>
-                    <option value="dogecoin">Dogecoin</option>
-                    <option value="solana">Solana</option>
-                    <option value="arbitrum">Arbitrum</option>
-                    <option value="optimism">Optimism</option>
-                </select>
+                    <div className="dropdown-subcontainer">
+                        <select className='dropdown-select' value={dropdownSelect} onChange={(e) => setDropdownSelect(e.target.value)}>
+                            <option value="bitcoin">Bitcoin</option>
+                            <option value="filecoin">Filecoin</option>
+                            <option value="ethereum">Ethereum</option>
+                            <option value="dogecoin">Dogecoin</option>
+                            <option value="solana">Solana</option>
+                            <option value="arbitrum">Arbitrum</option>
+                            <option value="optimism">Optimism</option>
+                        </select>
+                    </div>
                 </div>
-                <p className='chart-heading'>
-                    Price Chart - {dropdownSelect[0].toUpperCase()+ dropdownSelect.slice(1)}
-                </p>
-                <div id="lineChartMainDiv"className="chartContainer"
-                style={{width: '100wh', height:'500px'}}>
-                    
+                <div className="text-main-div">
+                    <div className='circle-div'></div>
+                    <h4> Monthly Active Addresses</h4>
                 </div>
 
-                
+                <div id="lineChartMainDiv" className="chartContainer"
+                    style={{ width: '100wh', height: '500px' }}>
 
-
+                </div>
             </div>
         </>
     )
